b25180401550b8fb0281b08c23c5250b97595c07
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2022-03-07 02:05:02 +0100

    work in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    this.socket = io('localhost:8000')
+  };
 
   render() {
     return (
       <div className="App">
-        Hello world
-      </div>
+
+      <header>
+        <h1>ToDoList.app</h1>
+      </header>
+  
+      <section className="tasks-section" id="tasks-section">
+        <h2>Tasks</h2>
+  
+        <ul className="tasks-section__list" id="tasks-list">
+          <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
+          <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+        </ul>
+  
+        <form id="add-task-form">
+          <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <button className="btn" type="submit">Add</button>
+        </form>
+  
+      </section>
+    </div>
     );
   };
 
--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 
 const app = express();
+
+const socket = require('socket.io');
+
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('a user is connected');
+
+  //updateData
+  socket.emit('updateData', tasks);
+
+  //addTask
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (id) => {
+    tasks.splice(tasks.findIndex((task) => task.id === id), 1);
+    socket.broadcast.emit('removeTask', id);
+  });
 });
 
 app.use((req, res) => {
